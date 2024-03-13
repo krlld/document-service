6557a1f965a0b985f51edf8537ba56dbe9d60054
krlld <kirilldikun25@gmail.com>
2024-03-13 21:51:37 +0300

    refactor: change method of createLimingDocument from get to post

--- src/main/java/com/example/documentservice/controller/DocumentController.java
+++ src/main/java/com/example/documentservice/controller/DocumentController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class DocumentController {
 
     public final LimingDocumentService limingDocumentService;
 
-    @GetMapping(value = "/liming-document", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @PostMapping(value = "/liming-document", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public Resource createLimingDocument(@Valid @RequestBody LimingRequestDto limingRequestDto) {
         return limingDocumentService.createDocument(limingRequestDto);
     }
